2104174e71e5a0e43306c52b278c9c32b6d561fd
SAyaanHussain <syedayaanhussain700@gmail.com>
2025-04-14 22:52:23 +0530

    Error fixed

--- static/js/sign.js
+++ static/js/sign.js
@@ -3,12 +3,12 @@ function togglePasswordVisibility(inputId, iconId) {
     const icon = document.getElementById(iconId);
 
     if (input.type === "password") {
-        input.type = "text"; // Change the input field type to 'text'
-        icon.classList.remove("bi-eye-slash"); // Remove 'eye-slash' icon
-        icon.classList.add("bi-eye"); // Add 'eye' icon
+        input.type = "text"; 
+        icon.classList.remove("bi-eye-slash"); 
+        icon.classList.add("bi-eye"); 
     } else {
-        input.type = "password"; // Change the input field type back to 'password'
-        icon.classList.remove("bi-eye"); // Remove 'eye' icon
-        icon.classList.add("bi-eye-slash"); // Add 'eye-slash' icon
+        input.type = "password";
+        icon.classList.remove("bi-eye"); 
+        icon.classList.add("bi-eye-slash"); 
     }
 }
